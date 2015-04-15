5d9333b36fc31fccd3fee2442676e4184e485be7
adlhenry <adlhenry@ucsc.edu>
2015-04-15 14:15:07 -0700

    Refactor

--- asg4/symtable.cpp
+++ asg4/symtable.cpp
@@ -77,20 +77,7 @@ void sym_print (const string *name, symbol *sym) {
 	printf ("\n");
 }
 
-/*attr_bitset set_attr (astree *node, astree *type, astree *ident) {
-	attr_bitset attributes = 0;
-	attributes[ATTR_variable] = 1;
-	attributes[ATTR_lval] = 1;
-	if (type->symbol == TOK_ARRAY) {
-		attributes[ATTR_array] = 1;
-		type = type->children[0];
-		ident = type->children[1];
-	}
-	attributes[attr_type[type->symbol]] = 1;
-	return attributes;
-}*/
-
-symbol *intern_symtable (const string *key, symbol *val) {
+void intern_symtable (const string *key, symbol *val) {
 	symbol_table *table = symbol_stack.back();
 	if (table == NULL) {
 		symbol_stack.pop_back();
@@ -103,21 +90,16 @@ symbol *intern_symtable (const string *key, symbol *val) {
 			errprintf (
 				"%: identifier previously declared: %s (%ld.%ld.%ld)\n",
 				key->c_str(), val->filenr, val->linenr, val->offset);
-			return NULL;
 		} else {
 			(*table)[key] = val;
 			sym_print (key, val);
 		}
 	}
-	return val;
 }
 
-void define_struct () {
-	
-}
-
-symbol_entry define_ident (astree *type) {
+symbol *define_ident (astree *type, int attr) {
 	attr_bitset attributes = 0;
+	if (attr) attributes[attr] = 1;
 	attributes[ATTR_variable] = 1;
 	attributes[ATTR_lval] = 1;
 	astree *ident = type->children[0];
@@ -132,7 +114,11 @@ symbol_entry define_ident (astree *type) {
 	symbol *var = new_symbol (ident);
 	var->attributes = attributes;
 	intern_symtable (key, var);
-	return make_pair (key, var);
+	return var;
+}
+
+void define_struct () {
+	
 }
 
 void define_func (astree *node) {
@@ -150,58 +136,16 @@ void define_func (astree *node) {
 	const string *key = ident->lexinfo;
 	symbol *func = new_symbol (ident);
 	func->attributes = attributes;
-	func = intern_symtable (key, func);
+	intern_symtable (key, func);
 	
 	new_block();
 	astree *param = node->children[1];
-	symbol_entry entry = define_ident (param->children[0]);
-	symbol *last = entry.second;
-	last->attributes[ATTR_param] = 1;
-	sym_print (entry.first, entry.second);
+	symbol *last = define_ident (param->children[0], ATTR_param);
 	func->parameters = last;
 	for (size_t child = 1; child < param->children.size(); child++) {
-		last->attributes[ATTR_param] = 1;
-		entry = define_ident (param->children[child]);
-		last->parameters = entry.second;
+		last->parameters = 
+		define_ident (param->children[child], ATTR_param);
 		last = last->parameters;
-		sym_print (entry.first, entry.second);
-	}
-	last->attributes[ATTR_param] = 1;
-	last->parameters = NULL;
-}
-
-void define_var (astree *node) {
-	attr_bitset attributes = 0;
-	attributes[ATTR_variable] = 1;
-	attributes[ATTR_lval] = 1;
-	astree *type = node->children[0];
-	astree *ident = type->children[0];
-	if (type->symbol == TOK_ARRAY) {
-		attributes[ATTR_array] = 1;
-		type = type->children[0];
-		ident = type->children[1];
-	}
-	attributes[attr_type[type->symbol]] = 1;
-	
-	const string *key = ident->lexinfo;
-	symbol *var = new_symbol (ident);
-	var->attributes = attributes;
-	symbol_table *table = symbol_stack.back();
-	if (table == NULL) {
-		symbol_stack.pop_back();
-		table = new symbol_table();
-		(*table)[key] = var;
-		symbol_stack.push_back (table);
-		sym_print (key, var);
-	} else {
-		if ((*table)[key] != NULL) {
-			errprintf (
-				"%: identifier previously declared: %s (%ld.%ld.%ld)\n",
-				key->c_str(), var->filenr, var->linenr, var->offset);
-		} else {
-			(*table)[key] = var;
-			sym_print (key, var);
-		}
 	}
 }
 
@@ -239,7 +183,7 @@ static int scan_node (astree *node) {
 		case TOK_PROTOTYPE:
 			break;
 		case TOK_VARDECL:
-			define_var (node);
+			define_ident (node->children[0], 0);
 			break;
 		case TOK_IDENT:
 			ref_ident (node);
