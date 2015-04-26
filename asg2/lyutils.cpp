a4e7c2d307f25ad4684b5f7c1a79364fcc131af3
adlhenry <adlhenry@ucsc.edu>
2015-04-26 11:52:41 -0700

    Format fixes

--- asg2/lyutils.cpp
+++ asg2/lyutils.cpp
@@ -54,19 +54,19 @@ void scanner_useraction (void) {
 void yyerror (const char *message) {
 	assert (not included_filenames.empty());
 	errprintf ("%: %s: %d: %s\n",
-				included_filenames.back().c_str(), scan_linenr, message);
+		included_filenames.back().c_str(), scan_linenr, message);
 }
 
 void scanner_badchar (unsigned char bad) {
 	char char_rep[16];
 	sprintf (char_rep, isgraph ((int) bad) ? "%c" : "\\%03o", bad);
 	errprintf ("%: %s: %d: invalid source character (%s)\n",
-				included_filenames.back().c_str(), scan_linenr, char_rep);
+		included_filenames.back().c_str(), scan_linenr, char_rep);
 }
 
 void scanner_badtoken (char *lexeme) {
 	errprintf ("%: %s: %d: invalid token (%s)\n",
-				included_filenames.back().c_str(), scan_linenr, lexeme);
+		included_filenames.back().c_str(), scan_linenr, lexeme);
 }
 
 // Print the token
